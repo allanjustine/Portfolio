f043b5713918006c57554a7dcfbb650b264a6f5a
Allan Justine Mascariñas <90177495+allanjustine@users.noreply.github.com>
2024-08-15 17:14:10 +0800

    portfolio commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+  darkMode: "class", // or 'media' for automatic dark mode based on system settings
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
